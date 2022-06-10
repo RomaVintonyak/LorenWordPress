d6f8adb1c997f9e8aedd503c5ad5671a9941b0a6
RomaVintonyak <19roman23@gmail.com>
2022-06-10 20:07:17 +0300

    create services sction

--- front-page.php
+++ front-page.php
@@ -6,36 +6,18 @@
 ?>
 <?php get_header(); ?>
 <?php get_template_part( 'templates/intro'); ?>
-<?php/**Main Content home page */?>
 
   <!--about us-->
   <section class="about mt-6" id="about">
     <div class="container">
       <div class="about__row">
         <div class="about__row--text">
-          <h2>Нас обирають</h2>
-          <p>
-            Весілля &ndash; важливий день в житті і ти точно хочеш щоб воно було ідеальним. 
-            Щоб цей важливий день пройшов на одному диханні, без зайвих хвилювань 
-            і форс мажорів тобі варто обирати професіоналів. Людей які мають досвід і 
-            знають всі підводні камені, які набили собі не одну шишку і готові зробити 
-            твоє свято особливим. 
-          </p>
-          <p>
-            Наша студія весільного декору почала працювати з 2016 року, 
-            за цей період ми сильно виросли і вдосконалилась, ми знаємо 
-            багато нюансів і маємо свої секрети.
-          </p>
-          <p>
-            Основне наше завдання &ndash; це втілити в життя Ваші мрії, щоб 
-            ідеальний декор з картинок ожив. Ми працюємо на результат а головний 
-            результат нашої роботи &ndash; задоволені клієнти. Основне наше 
-            правило &ndash; перевершити очікування! Ми робимо все і трішки більше.
-          </p>
+          <h2><?php the_field('zagolovok_bloku_nas_obyrayut'); ?></h2>
+            <?php the_field('opys_bloku_nas_obyrayut'); ?>
         </div><!--/.text-->
         <div class="about__row--img">
           <div class="about__row--iWraper">
-            <img src="img/about/3.jpg" alt="weding" class="img">
+            <img src="<?php the_field('zobrazhennya_bloku_nas_obyrayut'); ?>" alt="weding" class="img">
           </div><!--/.iWraper-->
         </div><!--/.img-->
       </div><!--/.about__row-->
@@ -47,101 +29,40 @@
       <div class="services__row">
         <div class="services__col services__col--img">
           <div class="services__slider" id="servicesSlider">
-            <img src="img/services/1.jpg" alt="wedding" class="img">
-            <img src="img/services/2.jpg" alt="wedding" class="img">
-            <img src="img/services/3.jpg" alt="wedding" class="img">
-            <img src="img/services/4.jpg" alt="wedding" class="img">
+            <?php if( have_rows('slajder_bloku_poslug') ): ?>
+              <?php while( have_rows('slajder_bloku_poslug') ): the_row(); ?>
+                <img src="<?php the_sub_field('zobrazhennya_bloku_poslug'); ?>" alt="wedding" class="img">
+              <?php endwhile; ?>
+            <?php endif; ?>
           </div>
         </div><!--/.img-->
         <div class="services__col services__col--text">
-          <h2>Ми пропонуємо</h2>
+          <h2><?php the_field('poslugy_zagolovok'); ?></h2>
           <div class="services__text">
-            <div class="services__text--col">
-              <div class="text__icon">
-                <img class="img" src="img/services/icons/8.svg" alt="icon">
-              </div><!--/.text__icon-->
-              <div class="text__data wow fadeInUp" data-wow-duration="2s" data-wow-offset="100">
-                <h3>Флористика</h3>
-                <p>
-                  Флористика — Це важлива складова весільного декору. 
-                  Нею не слід нехтувати. зону молодих,  Ми допоможемо 
-                  підібрати квіти відповідно до сезону, кольорової 
-                  гами та Ваших вподобань.
-                </p>
-              </div><!--/.text__data-->
-            </div><!--/.services__text-->
-            <div class="services__text--col">
-              <div class="text__icon">
-                <img class="img" src="img/services/icons/3.svg" alt="icon">
-              </div><!--/.text__icon-->
-              <div class="text__data wow fadeInUp" data-wow-duration="2s" data-wow-offset="100">
-                <h3>Зона молодих</h3>
-                <p>
-                  Варіантів оформлення існує безліч, 
-                  розпочинаючи матеріалами які використовуються 
-                  закінчуючи текстурою та кольоровою гамою. 
-                  Все це Ви можете обрати при зустрічі.
-                </p>
-              </div><!--/.text__data-->
-            </div><!--/.services__text-->
-            <div class="services__text--col">
-              <div class="text__icon">
-                <img class="img" src="img/services/icons/9.svg" alt="icon">
-              </div><!--/.text__icon-->
-              <div class="text__data wow fadeInUp" data-wow-duration="2s" data-wow-delay="1s" data-wow-offset="80">
-                <h3>Фотозона</h3>
-                <p>
-                  Фотозона - місце, що залишиться на світлинах 
-                  усіх гостей, потрібно подбати про розташування 
-                  фотозони та світлові ефекти, щоб фото вийшли ідеальні.
-                </p>
-              </div><!--/.text__data-->
-            </div><!--/.services__text-->
-            <div class="services__text--col">
-              <div class="text__icon">
-                <img class="img" src="img/services/icons/1.svg" alt="icon">
-              </div><!--/.text__icon-->
-              <div class="text__data wow fadeInUp" data-wow-duration="2s" data-wow-delay="1s" data-wow-offset="80">
-                <h3>Виїзна реєстрація</h3>
-                <p>
-                  Виїзна реєстрація - це без сумніву 
-                  найромантичніша частина Вашого свята. 
-                  Арка на фоні мальовничого краєвиду, 
-                  обов'язково доповнить церемонію 
-                  одруження і запам'ятається Вам на все життя.
-                </p>
-              </div><!--/.text__data-->
-            </div><!--/.services__text-->
-            <div class="services__text--col">
-              <div class="text__icon">
-                <img class="img" src="img/services/icons/7.svg" alt="icon">
-              </div><!--/.text__icon-->
-              <div class="text__data wow fadeInUp" data-wow-duration="2s" data-wow-delay="2s" data-wow-offset="60">
-                <h3>Весільна поліграфія</h3>
-                <p>
-                  Погодьтесь, перше враження про 
-                  свято з'являється уже після отримання 
-                  запрошення, розглянувши яке, 
-                  Ви чітко зрозумієте концепцію 
-                  весілля, стиль та кольорову гаму.
-                </p>
-              </div><!--/.text__data-->
-            </div><!--/.services__text-->
-            <div class="services__text--col">
-              <div class="text__icon">
-                <img class="img" src="img/services/icons/4.svg" alt="icon">
-              </div><!--/.text__icon-->
-              <div class="text__data wow fadeInUp" data-wow-duration="2s" data-wow-delay="2s" data-wow-offset="60">
-                <h3>Оформлення під ключ</h3>
-                <p>
-                  Якщо Ви готові довірити своє свято 
-                  професіоналам то ця послуга саме 
-                  для Вас! Ми допоможемо розробити 
-                  концепцію свята, підберемо декор 
-                  та флористику, з урахуванням Ваших побажань.
-                </p>
-              </div><!--/.text__data-->
-            </div><!--/.services__text-->
+            <?php
+              $args = array(
+                'posts_per_page'  => 6,
+                'post_type'       => 'post',
+                'category_name'   => 'Пости пропозицій'
+                );
+              ?>
+              <?php $mainPostServices = new WP_Query( $args, 'cat=6' ); ?>
+                <?php if ( $mainPostServices->have_posts() ) : ?>
+                    <?php while ( $mainPostServices->have_posts() ) : $mainPostServices->the_post(); ?>
+                    <div class="services__text--col">
+                      <div class="text__icon">
+                        <img class="img" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="icon">
+                      </div><!--/.text__icon-->
+                      <div class="text__data wow fadeInUp" data-wow-duration="2s" data-wow-offset="100">
+                        <h3><?php the_title(); ?></h3>
+                        <p>
+                          <?php the_field('osnovnyj_tekst_posta'); ?>
+                        </p>
+                      </div><!--/.text__data-->
+                    </div><!--/.services__text__coll-->
+                    <?php endwhile; ?>
+                    <?php wp_reset_postdata(); ?>
+                <?php endif; ?>
           </div><!--/.services__text-->
         </div><!--/.text-->
       </div><!--/.services__row-->
